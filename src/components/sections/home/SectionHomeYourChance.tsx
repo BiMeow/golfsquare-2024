aa96601753a7fb986fbda8c211469450e20bf52f
BiMeow <trungnguyen9879@gmail.com>
2024-01-05 22:52:53 +0700

    feat(Fe): page home

---
+++ src/components/sections/home/SectionHomeYourChance.tsx
@@ -0,0 +1,89 @@
+import gsap from "gsap";
+import { useRouter } from "next/router";
+import { memo, useEffect } from "react";
+
+let listPromo = [
+	{
+		title: "Promotion 1",
+		description:
+			"Redeemable on Topgolf game play, food and drinks, Lifetime Memberships, lessons and gear.",
+	},
+	{
+		title: "Promotion 2",
+		description: "Gift cards can be redeemed at any Topgolf location in the United States.",
+	},
+	{
+		title: "Promotion 3",
+		description: "Send via email or get a traditional gift card via mail.",
+	},
+];
+
+function SectionHomeYourChance({ ...props }) {
+	const router = useRouter();
+
+	useEffect(() => {
+		setTimeout(() => {
+			gsap.timeline({
+				scrollTrigger: {
+					trigger: ".SectionHomeYourChance",
+					start: "top bottom",
+					end: "bottom top",
+					scrub: 4,
+				},
+			}).fromTo(".SectionHomeYourChance .moveBanner", { y: 300 }, { y: 0 });
+		}, 1500);
+	}, []);
+
+	return (
+		<>
+			<div className={`SectionHomeYourChance relative`}>
+				<img
+					src="/images/home/yourchance-banner.jpg"
+					alt=""
+					className="moveBanner absolute top-0 aspect-[144/77] w-full rounded-b-[75px] object-cover"
+				/>
+				<div className="cusContainer relative">
+					<h2 className="titleStoke relative z-10 mb-[80px] text-center font-GilroyBold text-[170px] font-bold">
+						GRAB YOUR <br /> CHANCE
+					</h2>
+
+					<div className="listPromo mx-[-40px] mb-[80px] flex">
+						{listPromo.map((e: any, i: number) => (
+							<div
+								className="itemPromo w-1/3 px-[40px] text-center font-medium"
+								key={i}
+							>
+								<p className="mb-[20px] text-[24px]">{e.title}</p>
+								<p>{e.description}</p>
+							</div>
+						))}
+					</div>
+
+					<div className="mb-[80px] flex justify-center">
+						<button className="mainBtn">all promos</button>
+					</div>
+
+					<img
+						src="/images/home/yourchance-banner-2.jpg"
+						alt=""
+						className="mx-auto aspect-[144/77] w-full max-w-[1110px] rounded-[50px]"
+					/>
+				</div>
+			</div>
+
+			<style jsx global>{`
+				.SectionHomeYourChance {
+					.titleStoke {
+						background: transparent;
+						-webkit-background-clip: text;
+						-webkit-text-fill-color: transparent;
+						-webkit-text-stroke-width: 2px;
+						-webkit-text-stroke-color: white;
+					}
+				}
+			`}</style>
+		</>
+	);
+}
+
+export default memo(SectionHomeYourChance);
